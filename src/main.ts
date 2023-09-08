896424ca623ca943c06422695b40842c15a82bba
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-08 13:55:16 -0600

    fix(package.json): 🐛 Fix problem with lecture of variable NODE_ENV

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  console.info(configService.get('PORT'));
   await app.listen(3000);
 }
 bootstrap();
