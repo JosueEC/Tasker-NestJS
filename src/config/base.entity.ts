09545771aa595f9441d1713bc26e10bb4bf33a35
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-14 18:59:51 -0600

    feat(config): 🔨 Add the abstract class base.entity

---
+++ src/config/base.entity.ts
@@ -0,0 +1,24 @@
+import {
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  // Este decorador es para definir columnas que van a almacenar
+  // los datos de fecha de creacion y actualizacion de el registro
+  @CreateDateColumn({
+    type: 'timestamp',
+    name: 'created_at',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    name: 'updated_at',
+  })
+  updatedAt: Date;
+}
