8aa7492769089440c91235d46e55d29ce88653e2
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-14 20:50:40 -0600

    feat(user): 🔨 Add user.entity with tyrpeorm constraints

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,54 @@
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from 'src/config/base.entity';
+import { IUser } from 'src/interfaces/user.interface';
+
+@Entity({ name: 'user' })
+export class UserEntity extends BaseEntity implements IUser {
+  @Column({
+    name: 'firstName',
+    type: 'varchar',
+    length: 255,
+  })
+  firstName: string;
+
+  @Column({
+    name: 'lastName',
+    type: 'varchar',
+    length: 255,
+  })
+  lastName: string;
+
+  @Column({
+    name: 'age',
+    type: 'smallint',
+  })
+  age: number;
+
+  @Column({
+    name: 'email',
+    type: 'varchar',
+    length: 255,
+  })
+  email: string;
+
+  @Column({
+    name: 'username',
+    type: 'varchar',
+    length: 255,
+  })
+  username: string;
+
+  @Column({
+    name: 'password',
+    type: 'varchar',
+    length: 255,
+  })
+  password: string;
+
+  @Column({
+    name: 'role',
+    type: 'varchar',
+    length: 255,
+  })
+  role: string;
+}
