eeac73821ec69b602e20faa5febceb6c36becaa9
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-16 16:34:23 -0600

    feat(project.module): 🔨 Add new imports, controllers and providers

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectEntity } from './entities/project.entity';
+import { ProjectController } from './controllers/project.controller';
+import { ProjectService } from './services/project.service';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([ProjectEntity])],
+  controllers: [ProjectController],
+  providers: [ProjectService],
   exports: [],
 })
 export class ProjectModule {}
