a698204d26653fd3e794d1666d8cb3b2a10ee45d
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-16 13:52:22 -0600

    fix(user.controller): 🐛 Add basic CRUD routes for the UserService functions

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Param,
+  Get,
+  Post,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from '../services/user.service';
+import { CreateUserDto, UpdateUserDto } from '../dto';
+import { UserEntity } from '../entities/user.entity';
+import { DeleteResult, UpdateResult } from 'typeorm';
 
-@Controller('v1/user')
-export class UserController {}
+@Controller('/v1/user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  public async createUser(@Body() body: CreateUserDto): Promise<UserEntity> {
+    return this.userService.create(body);
+  }
+
+  @Get()
+  public async getAllUsers(): Promise<UserEntity[]> {
+    return await this.userService.findAll();
+  }
+
+  @Get(':id')
+  public async getOneUser(@Param('id') id: string): Promise<UserEntity> {
+    return await this.userService.findById(id);
+  }
+
+  @Patch(':id')
+  public async updateOneUser(
+    @Param('id') id: string,
+    @Body() body: UpdateUserDto,
+  ): Promise<UpdateResult> {
+    return await this.userService.updateById(id, body);
+  }
+
+  @Delete(':id')
+  public async deleteOneUser(@Param('id') id: string): Promise<DeleteResult> {
+    return this.userService.deleteById(id);
+  }
+}
