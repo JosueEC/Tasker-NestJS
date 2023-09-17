b98b0e5368c6a9454096b424c128cd494684d219
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-16 18:51:40 -0600

    build(types): 🔧 Add types.d.ts file to access to the environment variables

---
+++ types/index.d.ts
@@ -0,0 +1,20 @@
+// NodeJS.ProcessEnv
+
+// Este es un truco que se puede hacer con TypeScript en el cual
+// nos permite acceder a las variables de entorno desde el
+// objeto process.env con la dot notation
+
+// Basicamente con el namespace traemos todo lo que ya contiene
+// el objeto NodeJS, y a traves de la interface es como añadimos
+// los nuevos valores a los que deseamos poder acceder, que en este
+// caso son las variables de entorno
+declare namespace NodeJS {
+  interface ProcessEnv {
+    PORT: string;
+    DB_HOST: string;
+    DB_PORT: string;
+    DB_USER: string;
+    DB_PASSWORD: string;
+    DB_NAME: string;
+  }
+}
