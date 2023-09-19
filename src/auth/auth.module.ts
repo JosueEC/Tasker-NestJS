65ac3ae8d0ac77d8a5ab8a66c9e387c813708b9c
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-19 13:10:45 -0600

    feat(auth): 🔨 Generate the auth.module and add it's imports, controllers, providers and exports

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module, Global } from '@nestjs/common';
+import { AuthController } from './controllers/auth.controller';
+import { AuthService } from './services/auth.service';
+import { UserService } from 'src/user/services/user.service';
+import { UserModule } from 'src/user/user.module';
+
+@Global()
+// Este decorador nos permite dar acceso a un modulo de forma
+// global, de esta forma no sera necesario declararlo en los
+// demas modulos, ya que este podra ser accesible desde toda
+// la aplicacion. Esto es util cuando hay alguna servicio que
+// recurrentemente va a ser usado en otros modulos
+@Module({
+  imports: [UserModule],
+  controllers: [AuthController],
+  providers: [AuthService, UserService],
+  exports: [],
+})
+export class AuthModule {}
