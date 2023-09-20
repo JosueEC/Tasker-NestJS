c5150d723db9de2b259fd82841a293b2ae111c45
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-20 13:44:46 -0600

    fix(types/express): 🐛 Fix problem with the rolUser type

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -1,8 +1,10 @@
-import { ROLE } from 'src/constants';
-
+//* NOTA: En los declare no podemos agregar tipos personalizados
+//* para tipar las propiedades agregadas a la interface, estos
+//* deben ser tipados con tipos nativos, de lo contrario no seran
+//* reconocidos al llamarlos en la instancia
 declare namespace Express {
   interface Request {
     idUser: string;
-    roleUser: ROLE;
+    roleUser: string;
   }
 }
