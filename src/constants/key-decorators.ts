021d8ffe34d11fc7889c21a7948a973492955636
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-20 18:40:11 -0600

    feat(constants): 🔨 Add ROLES_KEY, ADMIN_KEY and ACCESS_LEVEL_KEY to the key.decorators.ts

--- src/constants/key-decorators.ts
+++ src/constants/key-decorators.ts
@@ -1,1 +1,8 @@
+// Las keys de los decoradores nos permiten identificarlos, estas
+// se usan en la creacion del decorador, y asi mismo, podemos
+// obtener el decorador en guard a traves de su key enviada en la
+// funcion .get<> del reflector
 export const PUBLIC_KEY = 'PUBLIC';
+export const ROLES_KEY = 'ROLES';
+export const ADMIN_KEY = 'ADMIN';
+export const ACCESS_LEVEL_KEY = 'ACCESS_LEVEL';
