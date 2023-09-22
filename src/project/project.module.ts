f18165c815564d11984db047463f9c1fb15be4e2
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-21 19:54:18 -0600

    feat(project.module): 🔨 Add the UsersProjectsEntity

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -3,9 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectEntity } from './entities/project.entity';
 import { ProjectController } from './controllers/project.controller';
 import { ProjectService } from './services/project.service';
+import { UsersProjectsEntity } from 'src/user/entities/usersProjects.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProjectEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ProjectEntity, UsersProjectsEntity]),
+    UserModule,
+  ],
   controllers: [ProjectController],
   providers: [ProjectService],
   exports: [],
